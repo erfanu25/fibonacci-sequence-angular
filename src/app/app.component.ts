fc682f20b53d4ed649510cf61037aa92de9ede0f
Erfan <mderfan2@gmail.com>
2020-10-09 20:55:36 +0600

    Completed the task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fibonacci-sequence-angular';
+
+  nthNum: number;
+  tempNum: number;
+  result: number;
+  counter = 2;
+  message: string;
+
+  ngOnInit(): void {
+  }
+
+  generate(): any {
+    if (this.nthNum) {
+      this.tempNum = this.nthNum;
+      this.result = 1;
+      if (this.tempNum !== 1) {
+        const intr =  setInterval(() => {
+          this.result = this.fib(this.counter++);
+          if (--this.tempNum === 1) {
+            clearInterval(intr);
+          }
+        }, 3000);
+      }
+    } else {
+      this.message = 'Please Enter a Number';
+    }
+  }
+
+  fib(n: number): number {
+    if (n <= 1) {
+      return n;
+    } else {
+      return this.fib(n - 1) + this.fib(n - 2);
+    }
+  }
+
+  clear(): void {
+    this.result = null;
+    this.counter = 2;
+    this.tempNum = null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RadioButtonModule} from 'primeng/radiobutton';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TooltipModule} from 'primeng/tooltip';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RadioButtonModule,
+        FormsModule,
+        InputTextModule,
+        ButtonModule,
+        TooltipModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
